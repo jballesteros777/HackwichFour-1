255b22673b55c7771451e1bfa0c8f30fc1d2b86b
Jordan Ballesteros <pookele@gmail.com>
2019-04-05 10:29:06 -1000

    Completed Detail View

--- HackwichFour/ViewController.swift
+++ HackwichFour/ViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 //declare our array
 
-  
-    var myFriends = ["Ryan", "Nicole", "Sara", "Tyler"]
+    @IBOutlet var tableView: UITableView!
+    
+    var myFriends = ["Kapolei Kalapawai"]
 
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -27,19 +28,41 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let text = myFriends[indexPath.row]
         cell.textLabel?.text=text
         return cell
+        
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+        
     }
 
-
-
+    var restaurantImageData = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        
+        restaurantImageData = dict!.object(forKey: "restaurantImages") as! [String]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "mySegue"
+        {
+            let s1 = segue.destination as! detailViewController
+            let imageIndex = tableView.indexPathForSelectedRow?.row
+            s1.imagePass = restaurantImageData[imageIndex!]
+            
+        }
     }
 
 
---
+++ HackwichFour/detailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  detailViewController.swift
+//  HackwichFour
+//
+//  Created by Amber Ballesteros on 4/5/19.
+//  Copyright © 2019 Marion Ano. All rights reserved.
+//
+
+import UIKit
+
+class detailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    var imagePass: String?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        if let imageName = imagePass {
+            imageView.image = UIImage(named: imageName)
+        }
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
